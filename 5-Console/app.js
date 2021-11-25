947e1142bf1a820f4f23bcad286a30dd4eddb5ec
jhhg04 <jhhg04@gmail.com>
2021-11-24 20:19:32 -0500

    add

---
+++ 5-Console/app.js
@@ -0,0 +1,9 @@
+var a = 1;
+var b = 'John';
+
+console.log(a);
+console.warn(a);
+console.error(a);
+console.info(a);
+
+console.log(a + a);
---
+++ 6-Undefined/app.js
@@ -0,0 +1,16 @@
+/*
+    undefined === null // false - Diferentes Objetos
+    undefined == null // true - Ambos sin valor
+*/
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+var a = 'John';
+var b = 1;
+var c = true;
+
+console.log(a);
+console.log(b);
+console.log(c);
