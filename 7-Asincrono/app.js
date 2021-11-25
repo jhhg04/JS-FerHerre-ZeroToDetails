b447ad564b43871824cf1e4e19eb1237db62f60d
jhhg04 <jhhg04@gmail.com>
2021-11-24 21:51:38 -0500

    add

--- 7-Asincrono/app.js
+++ 7-Asincrono/app.js
@@ -1,3 +1,13 @@
 /*
-   
+  Asincrono : Solo tiene un hilo, hasta que no termine con la primera funcion no hace la otra
 */
+function imprimir() {
+  for (let i = 0; i < 8000; i++) {
+    console.log('Imprimio');
+  }
+}
+
+function presionoClick() {
+  console.log('Click en Boton');
+}
+imprimir();
